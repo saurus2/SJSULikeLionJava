254640b0cf4383c7141029642153644f1e8a4e11
Jooyul Yoon <juyeolyoon@gmail.com>
2023-09-26 12:07:52 -0700

    JPA setup

--- src/main/java/dev/sjsuJava/demo/DemoApplication.java
+++ src/main/java/dev/sjsuJava/demo/DemoApplication.java
@@ -2,11 +2,13 @@ package dev.sjsuJava.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@EnableJpaAuditing
 public class DemoApplication {
 	@RequestMapping("/")
 	String home(){
