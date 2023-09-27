60898b31e2e52f4db3c674b4d95d714ef246b68e
dasom <dasomlee923@gmail.com>
2023-09-26 23:38:35 -0700

    ãcreate_table, insert_data updated

--- database/scripts/create_table.sql
+++ database/scripts/create_table.sql
@@ -1,5 +1,36 @@
-CREATE TABLE users (
-user_id INT(11) NOT NULL AUTO_INCREMENT primary key,
-username varchar(25) unique not null,
-password varchar(25) not null
+CREATE TABLE USERS(
+    user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(20) UNIQUE NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    profile_pic_id VARCHAR(20)
+);
+
+CREATE TABLE POST(
+    post_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    writer INT NOT NULL,
+    post_title VARCHAR(30) NOT NULL,
+    post_date DATE NOT NULL,
+    post_pic_id VARCHAR(20) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    roars_count INT NOT NULL DEFAULT 0,
+    FOREIGN KEY (writer) REFERENCES USERS(user_id)
+);
+
+CREATE TABLE COMMENT(
+    comment_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    post_id INT NOT NULL,
+    writer INT NOT NULL,
+    comment_date DATE NOT NULL,
+    contents VARCHAR(40),
+    FOREIGN KEY (post_id) REFERENCES POST(post_id),
+    FOREIGN KEY (writer) REFERENCES USERS(user_id)
+);
+
+CREATE TABLE ROARS(
+    roar_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    post_id INT NOT NULL,
+    UNIQUE KEY (user_id, post_id), --Ensure a user can like a post only once
+    FOREIGN KEY (user_id) REFERENCES USERS(user_id),
+    FOREIGN KEY (post_id) REFERENCES POST(post_id)
 );
--- database/scripts/insert_data.sql
+++ database/scripts/insert_data.sql
@@ -1,2 +1,43 @@
-INSERT INTO users (username, password) VALUES ('admin', '1234');
-INSERT INTO users (username, password) VALUES ('test', '1234');
+-- Insert USERS
+INSERT INTO USERS (user_name, password) VALUES ('jooyul', 'likelion');
+INSERT INTO USERS (user_name, password) VALUES ('jihyeok', 'likelion');
+INSERT INTO USERS (user_name, password) VALUES ('jaewon', 'likelion');
+INSERT INTO USERS (user_name, password) VALUES ('yongjun', 'likelion');
+INSERT INTO USERS (user_name, password) VALUES ('dasom', 'likelion');
+
+-- Insert POST
+INSERT INTO POST (writer, post_title, post_date, post_pic_id, description)
+VALUES (1, 'Hi, everyone', '2023-09-26','odajdh395_nf', 'Welcome to the Lionstagram!');
+INSERT INTO POST (writer, post_title, post_date, post_pic_id, description)
+VALUES (5, 'Sharing my day', '2023-09-27', '939fjgjslsl1030000', 'I attended likelion meeting today');
+INSERT INTO POST (writer, post_title, post_date, post_pic_id, description)
+VALUES (3, 'Tried new korean restaurant', '2023-09-28', '1230599559fkg', 'The food was amazing!');
+
+-- Insert COMMENT
+INSERT INTO COMMENT (post_id, writer, comment_date, contents)
+VALUES (1, 4, '2023-09-27', 'Thank you for the warm welcome!');
+INSERT INTO COMMENT (post_id, writer, comment_date, contents)
+VALUES (2, 2, '2023-09-28', 'How was the meeting?');
+
+-- Insert ROARS for post 1
+INSERT INTO ROARS (user_id, post_id) VALUES (1, 1);
+INSERT INTO ROARS (user_id, post_id) VALUES (2, 1);
+
+-- Insert ROARS for post 2
+INSERT INTO ROARS (user_id, post_id) VALUES (3, 2);
+INSERT INTO ROARS (user_id, post_id) VALUES (4, 2);
+INSERT INTO ROARS (user_id, post_id) VALUES (5, 2);
+
+-- Insert ROARS for post 3
+INSERT INTO ROARS (user_id, post_id) VALUES (1, 3);
+
+-- Query roars_count for post 1
+SELECT roars_count FROM POST WHERE post_id = 1;
+
+-- Query roars_count for post 2
+SELECT roars_count FROM POST WHERE post_id = 2;
+
+-- Query roars_count for post 3
+SELECT roars_count FROM POST WHERE post_id = 3;
+
+-- The results of these queries above should show you the roars_count for each post
