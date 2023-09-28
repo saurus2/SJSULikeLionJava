e84f6286a7e8f56ecfd7f3d59a975baecbf0e26c
Jooyul Yoon <juyeolyoon@gmail.com>
2023-09-28 02:05:55 -0700

    dependency injection

--- src/main/java/dev/sjsuJava/demo/Service/CommentServiceImpl.java
+++ src/main/java/dev/sjsuJava/demo/Service/CommentServiceImpl.java
@@ -1,5 +1,11 @@
 package dev.sjsuJava.demo.Service;
 
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor // 의존성 자동 주입
 public class CommentServiceImpl {
     
 }
