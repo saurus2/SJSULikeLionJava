f7967580bf29ebf312c5f83234ccc68db8bd0137
Jooyul Yoon <juyeolyoon@gmail.com>
2023-09-28 10:40:07 -0700

    path control

--- src/main/java/dev/sjsuJava/demo/DemoApplication.java
+++ src/main/java/dev/sjsuJava/demo/DemoApplication.java
@@ -3,16 +3,23 @@ package dev.sjsuJava.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+@Controller
 @SpringBootApplication
 @EnableJpaAuditing
 public class DemoApplication {
 	@RequestMapping("/")
 	String home(){
-		return "Hello World!";
+		return "home";
+	}
+	@RequestMapping("/login")
+	String login(){
+		return "login";
+	}
+	@RequestMapping("/signup")
+	String signup(){
+		return "signup";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
