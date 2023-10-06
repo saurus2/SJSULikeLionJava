40cb430d7e03630165c4ad091505d48ed793776a
Jooyul Yoon <juyeolyoon@gmail.com>
2023-10-05 22:04:47 -0700

    feat: connect picture

--- src/main/java/dev/sjsuJava/demo/Service/PostServiceImpl.java
+++ src/main/java/dev/sjsuJava/demo/Service/PostServiceImpl.java
@@ -7,8 +7,10 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import dev.sjsuJava.demo.Dto.PostDto;
+import dev.sjsuJava.demo.Entity.Picture;
 import dev.sjsuJava.demo.Entity.Post;
 import dev.sjsuJava.demo.Entity.User;
+import dev.sjsuJava.demo.Repository.PictureRepository;
 import dev.sjsuJava.demo.Repository.PostRepository;
 import dev.sjsuJava.demo.Repository.UserRepository;
 import dev.sjsuJava.demo.exception.NotFoundException;
@@ -17,25 +19,32 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
-    private final UserRepository userRepository;
     private final PostRepository repository;
-    
+    private final UserRepository userRepository;
+    private final PictureRepository pictureRepository;
+
     @Override
     public PostDto upload(PostDto dto) {
 
         Optional<User> writer = userRepository.findById(dto.getWriter_id());
 
-        if(writer.isEmpty()) {
+        if (writer.isEmpty()) {
             throw new NotFoundException("Invalid Writer");
+        }
+
+        Optional<Picture> picture = pictureRepository.findById(dto.getPicture_id());
+
+        if (writer.isEmpty()) {
+            throw new NotFoundException("Invalid Picture");
         }
 
         Post post = Post.builder()
                 .post_id(dto.getPost_id())
-                .writer(userRepository.getReferenceById(dto.getWriter_id()))
+                .writer(writer.get())
                 .content(dto.getContent())
-                .pic_id(dto.getPic_id())
+                .picture(picture.get())
                 .roars_count(dto.getRoars_count())
-                .build();       
+                .build();
 
         return PostDto.from(repository.save(post));
     }
@@ -45,7 +54,7 @@ public class PostServiceImpl implements PostService {
         List<PostDto> postDtolist = new ArrayList<>();
         List<Post> postList = repository.findAll();
 
-        for(Post post : postList) {
+        for (Post post : postList) {
             postDtolist.add(PostDto.from(post));
         }
         return postDtolist;
@@ -54,24 +63,24 @@ public class PostServiceImpl implements PostService {
     @Override
     public PostDto readPost(int id) {
         Optional<Post> post = repository.findById((long) id);
-        if(post.isEmpty()) {
+        if (post.isEmpty()) {
             throw new NotFoundException();
         }
-        
+
         return PostDto.from(post.get());
     }
 
     @Override
     public List<PostDto> readPostByWriterId(int id) {
         Optional<User> writer = userRepository.findById((long) id);
-        if(writer.isEmpty()) {
+        if (writer.isEmpty()) {
             throw new NotFoundException();
         }
-        
+
         List<PostDto> postDtolist = new ArrayList<>();
         List<Post> postList = repository.findAllByWriter(writer.get());
 
-        for(Post post : postList) {
+        for (Post post : postList) {
             postDtolist.add(PostDto.from(post));
         }
         return postDtolist;
@@ -80,27 +89,29 @@ public class PostServiceImpl implements PostService {
     @Override
     public void update(int id, PostDto dto) {
         Optional<Post> OtargetPost = repository.findById((long) id);
-        if(OtargetPost.isEmpty()) {
+        if (OtargetPost.isEmpty()) {
             throw new NotFoundException();
         }
 
         Post targetPost = OtargetPost.get();
 
         Post post = Post.builder()
-            .post_id(dto.getPost_id() == null ? targetPost.getPost_id() : dto.getPost_id())
-            .writer(dto.getWriter_id() == null ? targetPost.getWriter() : userRepository.getReferenceById(dto.getWriter_id()))
-            .content(dto.getContent() == null ? targetPost.getContent() : dto.getContent())
-            .pic_id(dto.getPic_id() == null ? targetPost.getPic_id() : dto.getPic_id())
-            .roars_count(dto.getRoars_count() == null ? targetPost.getRoars_count() : dto.getRoars_count())
-        .build();
-        
+                .post_id(dto.getPost_id() == null ? targetPost.getPost_id() : dto.getPost_id())
+                .writer(dto.getWriter_id() == null ? targetPost.getWriter()
+                        : userRepository.getReferenceById(dto.getWriter_id()))
+                .content(dto.getContent() == null ? targetPost.getContent() : dto.getContent())
+                .picture(dto.getPicture_id() == null ? targetPost.getPicture()
+                        : pictureRepository.getReferenceById(dto.getPicture_id()))
+                .roars_count(dto.getRoars_count() == null ? targetPost.getRoars_count() : dto.getRoars_count())
+                .build();
+
         repository.save(post);
     }
 
     @Override
     public void delete(int id) {
         Optional<Post> post = repository.findById((long) id);
-        if(post.isEmpty()) {
+        if (post.isEmpty()) {
             throw new NotFoundException();
         }
 
