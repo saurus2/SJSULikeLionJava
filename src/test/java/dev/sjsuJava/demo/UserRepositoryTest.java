f74478215b94d6f75fe0560079a9c8650b42ae27
Jihyeok <bluesaurus2@gmail.com>
2023-10-21 12:39:52 -0700

    Revert "removed test"
    This reverts commit afb8d4060e0daa18750223f721994f44ec7e55e3.

---
+++ src/test/java/dev/sjsuJava/demo/UserRepositoryTest.java
@@ -0,0 +1,78 @@
+package dev.sjsuJava.demo;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import dev.sjsuJava.demo.Entity.User;
+import dev.sjsuJava.demo.Repository.UserRepository;
+
+@SpringBootTest
+public class UserRepositoryTest {
+    
+    
+    @Autowired
+    UserRepository userRepository;
+
+    // Insert Test
+    @Test
+    public void testInsertDummies(){
+    // 100개의 새로운 user객체를 생성하고 
+        IntStream.rangeClosed(1,10).forEach(i -> {
+           User user =  User.builder()
+                .username("guest"+i)
+                .password("1234")
+                .name("Yoon")
+                .build();
+            // 저장한다
+           userRepository.save(user);
+        });
+    }
+
+
+    // Select Test
+    @Test
+    public void testSelect(){
+        Long id = 5L;
+        Optional<User> result = userRepository.findById(id);
+        System.out.println("================");
+        if(result.isPresent()){
+            User user = result.get();
+            System.out.println(user.getUsername());
+        }
+    }
+
+    // Update
+    @Test
+    public void testUpdate(){
+        // 객체 생성
+        Long id = 4L;
+        User user = userRepository.getReferenceById(id);
+        user.setPassword("asdasd");
+        System.out.println("1-------------");
+        userRepository.save(user);
+        System.out.println("2-------------");
+    }
+
+    // Delete
+    @Test
+    public void testDelete(){
+        Long id = 7L;
+        userRepository.deleteById(id);
+    }
+
+    // Authenticate
+    @Test
+    public void testAuthenticate() {
+        Optional<User> user = userRepository.findByUsername("qwe");
+
+        Assertions.assertEquals(user.get().getUsername(), "qwe");
+        Assertions.assertEquals(user.get().getPassword(), "asd");
+    }
+
+
+}
